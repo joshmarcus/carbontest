9cd6ccf6f6ed964e857edf12b28467b74349063f
Adam Hinz <hinz.adam@gmail.com>
2012-11-10 17:18:26 +0000

    Support post params

--- src/main/scala/carbon/rest/Carbon.scala
+++ src/main/scala/carbon/rest/Carbon.scala
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.core.Response
 import javax.ws.rs.{GET, POST, Path, DefaultValue, QueryParam}
 import javax.ws.rs._
-import javax.ws.rs.core.{Response, Context, MediaType}
+import javax.ws.rs.core.{Response, Context, MediaType, MultivaluedMap}
 import geotrellis._
 import geotrellis.feature.Polygon
 import geotrellis.process._
@@ -17,8 +17,9 @@ object Carbon {
 
   def init = {
     // Load and cache tileset for future requests.
-    val uncachedRaster = Raster.loadUncachedTileSet("/var/trellis/carbon", server)
-    val raster = Raster.loadTileSet("/var/trellis/carbon", server)
+    var carbonLocation = "/var/geotrellis/data/arg/carbon"
+    val uncachedRaster = Raster.loadUncachedTileSet(carbonLocation, server)
+    val raster = Raster.loadTileSet(carbonLocation, server)
 
     val tileSetRD = raster.data.asInstanceOf[TileArrayRasterData] 
     val tileSums = stat.TiledPolygonalZonalCount.createTileSums(tileSetRD, raster.rasterExtent)
@@ -30,10 +31,6 @@ object Carbon {
 }
 
 
-/*
-[[[46.8987059660001,-72.9305432739999],[46.8603568440001,-72.9026982369999],[46.8701684350001,-72.8718149719999],[46.8582367130001,-72.8503932059999],[46.8540188550001,-72.8106433749999],[46.8601017120001,-72.7931613979999],[46.8443175970001,-72.7652966349999],[46.7743482080001,-72.8005041699999],[46.7507046780001,-72.7945144059999],[46.7277581980001,-72.7661873269999],[46.7387796160001,-72.7890890649999],[46.7275732720001,-72.8051887809999],[46.7217330760001,-72.7965863709999],[46.6993313750001,-72.8327215129999],[46.6947933020001,-72.8262883529999],[46.6853179800001,-72.8403467879999],[46.6788833820001,-72.8323560439999],[46.6696854910001,-72.8461672849999],[46.6610962430001,-72.8332544419999],[46.6510625530001,-72.8436434819999],[46.6464987870001,-72.8520730299999],[46.6555432620001,-72.8661898859999],[46.6639057520001,-72.8591963619999],[46.6731795260001,-72.8782019609999],[46.7239680570001,-72.9090768969999],[46.7336678200001,-72.9270037009999],[46.7455925620001,-72.9938159559999],[46.7363134210001,-73.0115222509999],[46.6831655030001,-72.9869977249999],[46.6615663270001,-72.9900152899999],[46.6496921660001,-72.9640384449999],[46.6489143340001,-72.9993395169999],[46.6530826500001,-73.0320505839999],[46.7067260780001,-73.0641309569999],[46.7041270210001,-73.1068509759999],[46.7508603320001,-73.1789583839999],[46.7645231160001,-73.1693505189999],[46.7705906670001,-73.1437543979999],[46.7867733000001,-73.1321568659999],[46.8329199780001,-73.1324262069999],[46.8283279460001,-73.1272984199999],[46.8357462580001,-73.1099951379999],[46.8507769270001,-73.1067260969999],[46.8662109760001,-73.0853116489999],[46.8863109240001,-73.0785434179999],[46.9020384780001,-73.0438867939999],[46.9176470430001,-73.0394216629999],[46.9186829380001,-73.0178932989999],[46.9088636110001,-73.0083853339999],[46.8988013160001,-72.9691029289999],[46.8987059660001,-72.9305432739999]]]
-*/
-
 /**
  * Demo rest endpoint for Vizzuality
  *
@@ -44,13 +41,14 @@ class Carbon {
   
   @POST
   def carbonPost(
-    polygonJson:String
+    params: MultivaluedMap[String,String]
   ) = {
+    var polygonJson = params.getFirst("polygon")
     if (polygonJson == null) {
       Response.serverError().entity("{ \"error\" => 'Carbon server has received an empty request.' }").`type`("application/json").build() 
     } else {
-    //println("received json: " + polygonJson)
-    //println("received POST request.")
+      println("received json: " + polygonJson)
+      println("received POST request.")
       carbon(polygonJson, "true", "true", "new", 4000)
     }
   }
